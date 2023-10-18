0e72ebe39d6c241e7115ed2163a20256b35274dd
dhruv.sonone <dhruv.sonone@pubmatic.com>
2023-10-18 12:25:30 +0530

    Adding setup_type validation

--- tasks/add_new_openwrap_partner.py
+++ tasks/add_new_openwrap_partner.py
@@ -1324,9 +1324,8 @@ def main():
       raise BadSettingException('OPENWRAP_USE_1x1_CREATIVE')
 
   video_position_type =  getattr(settings, 'VIDEO_POSITION_TYPE', None) 
-  if video_position_type is not None  and video_position_type not in (constant.PREROLL, constant.MIDROLL, constant.POSTROLL, constant.ALL) :
+  if video_position_type is not None and setup_type in (constant.ADPOD, constant.VIDEO) and video_position_type not in (constant.PREROLL, constant.MIDROLL, constant.POSTROLL, constant.ALL) :
      raise  BadSettingException('VIDEO_POSITION_TYPE should have one of these values: {preroll}, {midroll}, {postroll} or {all}'.format(preroll =constant.PREROLL, midroll = constant.MIDROLL, postroll = constant.POSTROLL, all = constant.ALL))
-
 
   custom_targeting = getattr(settings, 'OPENWRAP_CUSTOM_TARGETING', None)
   if custom_targeting != None:
